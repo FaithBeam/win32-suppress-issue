6de4f35329d7f34bdb77a71568aa2c92437d01a5
tech <pigeonhopper68@gmail.com>
2024-04-15 05:43:59 -0400

    send input

--- newtest/Program.cs
+++ newtest/Program.cs
@@ -1,15 +1,14 @@
 ﻿using System.Runtime.InteropServices;
 using Windows.Win32;
+using Windows.Win32.Foundation;
 using Windows.Win32.UI.Input.KeyboardAndMouse;
 using Windows.Win32.UI.WindowsAndMessaging;
-using WpfApp1;
 
 namespace newtest;
 
 class Program
 {
-    [STAThread]
-    static void Main(string[] args)
+    static unsafe void Main(string[] args)
     {
         var handle = PInvoke.GetModuleHandle((string?)null);
         var hhook = PInvoke.SetWindowsHookEx(
@@ -23,36 +22,27 @@ class Program
                 if (param == PInvoke.WM_XBUTTONDOWN || param == PInvoke.WM_XBUTTONUP)
                 {
                     var msll = Marshal.PtrToStructure<MSLLHOOKSTRUCT>(lParam);
-                    unsafe
+                    // switch on high-order word
+                    switch (((ushort*)&msll.mouseData)[1])
                     {
-                        // switch on high-order word
-                        switch (((ushort*)&msll.mouseData)[1])
-                        {
-                            case PInvoke.XBUTTON1:
-                                Console.WriteLine("XB1");
-                                var input = new INPUT { type = INPUT_TYPE.INPUT_MOUSE };
-                                input.Anonymous.mi.dx = 0;
-                                input.Anonymous.mi.dy = 0;
-                                if (PInvoke.GetCursorPos(out var point))
-                                {
-                                    input.Anonymous.mi.dx = point.X;
-                                    input.Anonymous.mi.dy = point.Y;
-                                }
-                                input.Anonymous.mi.dwFlags =
-                                    MOUSE_EVENT_FLAGS.MOUSEEVENTF_ABSOLUTE
-                                    | MOUSE_EVENT_FLAGS.MOUSEEVENTF_RIGHTDOWN
-                                    | MOUSE_EVENT_FLAGS.MOUSEEVENTF_RIGHTUP;
-                                input.Anonymous.mi.mouseData = 0;
-                                input.Anonymous.mi.dwExtraInfo = 0;
-                                input.Anonymous.mi.time = 0;
-                                PInvoke.SendInput(1, &input, sizeof(INPUT));
-                                return (Windows.Win32.Foundation.LRESULT)1;
-                            case PInvoke.XBUTTON2:
-                                Console.WriteLine("XB2");
-                                return (Windows.Win32.Foundation.LRESULT)1;
-                            default:
-                                throw new Exception();
-                        }
+                        case PInvoke.XBUTTON1:
+                            Console.WriteLine("XB1");
+
+                            // send rmb
+                            var input = new INPUT { type = INPUT_TYPE.INPUT_MOUSE };
+                            input.Anonymous.mi.dx = msll.pt.X;
+                            input.Anonymous.mi.dy = msll.pt.Y;
+                            input.Anonymous.mi.dwFlags =
+                                MOUSE_EVENT_FLAGS.MOUSEEVENTF_ABSOLUTE
+                                | MOUSE_EVENT_FLAGS.MOUSEEVENTF_RIGHTDOWN
+                                | MOUSE_EVENT_FLAGS.MOUSEEVENTF_RIGHTUP;
+                            input.Anonymous.mi.mouseData = 0;
+                            input.Anonymous.mi.dwExtraInfo = 0;
+                            input.Anonymous.mi.time = 0;
+                            PInvoke.SendInput(1, &input, sizeof(INPUT));
+
+                            // suppress xb1
+                            return (LRESULT)(-1);
                     }
                 }
                 return PInvoke.CallNextHookEx(null, code, param, lParam);
@@ -60,7 +50,10 @@ class Program
             handle,
             0
         );
-        var app = new App();
-        app.Run();
+        while (PInvoke.GetMessage(out var lpMsg, HWND.Null, 0, 0) != -1)
+        {
+            PInvoke.TranslateMessage(lpMsg);
+            PInvoke.DispatchMessage(lpMsg);
+        }
     }
 }
